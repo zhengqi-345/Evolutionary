f40eddabbd4efe70f4f84671528b6bd036ca84e1
zhengqi-345 <zhengq@umn.edu>
2018-04-09 12:59:15 -0500

    Update Futhest_Member_Selection.pl

--- Futhest_Member_Selection.pl
+++ Futhest_Member_Selection.pl
@@ -1,8 +1,34 @@
 #!/usr/bin/perl
 
-BEGIN{unshit @INC, "/path/to/"}
+BEGIN{unshit @INC, "/path/to/PhyTree.pm"};
+
 use warnings;
 use strict;
-use Getopts::Std;
+use POSIX qw(strftime);
+use Filename::Basename;
+use Getopt::Std;
 use vars qw($opt_i $opt_o);
-getopts
+getopts('i:o:');
+
+use PhyTree; ##use personel perl module
+
+if( $opt_i && $opt_o ){
+    print " Input: $opt_i\n";
+    print "Output: $opt_o\n";
+    print strftime(" Start: %Y-%m-%d %H:%M:%S\n",localtime(time));
+    
+    print strftime("   End: %Y-%m-%d %H-%M-%d")
+}else{
+    &usage;
+}
+
+sub usage{
+    die(
+        qq!
+        Usage: perl Furthest_Member_Selection.pl [ -i Input_file ] [ -o Output_file ]
+    Arguments: -i Input file name. File type is the output of blastp with "outfmt 6" (Required)
+               -o Output file name. File name provided is to store the selected members' sequences(Required).
+      Version: V1.0
+      \n!
+    )
+}
